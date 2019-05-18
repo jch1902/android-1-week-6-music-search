47bb9d5ccef7886364c63687916c802ef5930794
Jasper <jasper.d39c@gmail.com>
2019-05-17 21:31:01 -0700

    initial commit

--- app/src/main/java/com/ucsdextandroid1/musicsearch/data/DataSources.java
+++ app/src/main/java/com/ucsdextandroid1/musicsearch/data/DataSources.java
@@ -1,6 +1,29 @@
 package com.ucsdextandroid1.musicsearch.data;
 
+import android.util.Log;
+import androidx.annotation.NonNull;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 /**
  * Created by rjaylward on 2019-05-10
@@ -12,7 +35,20 @@ public class DataSources {
     private DataSources() {
 
         //TODO create a custom gson
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(ItunesSearchResults.class, new ItunesSearchResultsDeserialzer())
+                .addSerializationExclusionStrategy(new ExclusionStrategy() {
+                    @Override
+                    public boolean shouldSkipField(FieldAttributes f) {
+                        return f.getAnnotation(RemoveFromJson.class) != null;
+                    }
 
+                    @Override
+                    public boolean shouldSkipClass(Class<?> clazz) {
+                        return false;
+                    }
+                })
+                .create();
         //TODO use retrofit to create an instance of the itunesApi
     }
 
