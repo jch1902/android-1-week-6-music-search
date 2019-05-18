9234542617a2173f615dc5c26bd14adc4346efeb
extstudent <jasper.d39c@gmail.com>
2019-05-18 11:14:51 -0700

    initial commit

--- app/src/main/java/com/ucsdextandroid1/musicsearch/data/ItunesSongItem.java
+++ app/src/main/java/com/ucsdextandroid1/musicsearch/data/ItunesSongItem.java
@@ -14,21 +14,21 @@ public class ItunesSongItem implements SongItem {
     private String trackName;
     @SerializedName("artistName")
     private String artistName;
-    @SerializedName("albumName")
-    private String albumName;
     @SerializedName("artworkUrl60")
     private String artworkUrl;
     @SerializedName("previewUrl")
     private String previewUrl;
     @SerializedName("trackTimeMillis")
     private long trackTime;
+    @SerializedName("collectionName")
+    private String  collectionName;
 
     public ItunesSongItem(long trackId, String trackName, String artistName,
-                          String albumName, String artworkUrl, String previewUrl, long trackTime){
+                          String artworkUrl, String previewUrl, long trackTime, String collectionName){
         this.trackId = trackId;
         this.trackName = trackName;
         this.artistName = artistName;
-        this.albumName = albumName;
+        this.collectionName = collectionName;
         this.artworkUrl = artworkUrl;
         this.previewUrl = previewUrl;
         this.trackTime = trackTime;
@@ -50,7 +50,7 @@ public class ItunesSongItem implements SongItem {
 
     @Override
     public String getAlbumName() {
-        return albumName;
+        return collectionName;
     }
 
     @Override
