c8f819bb2144a823a37554bc2a4760145a8474df
Jasper <jasper.d39c@gmail.com>
2019-05-17 20:23:35 -0700

    initial commit

--- app/src/main/java/com/ucsdextandroid1/musicsearch/data/ItunesSongItem.java
+++ app/src/main/java/com/ucsdextandroid1/musicsearch/data/ItunesSongItem.java
@@ -8,40 +8,64 @@ import com.google.gson.annotations.SerializedName;
 public class ItunesSongItem implements SongItem {
 
     //TODO add all the members to this class with proper serialized names
+    @SerializedName("trackId")
+    private long trackId;
+    @SerializedName("trackName")
+    private String trackName;
+    @SerializedName("artistName")
+    private String artistName;
+    @SerializedName("albumName")
+    private String albumName;
+    @SerializedName("artworkUrl")
+    private String artworkUrl;
+    @SerializedName("previewUrl")
+    private String previewUrl;
+    @SerializedName("trackTimeMillis")
+    private long trackTime;
 
+    public ItunesSongItem(long trackId, String trackName, String artistName,
+                          String albumName, String artworkUrl, String previewUrl, long trackTime){
+        this.trackId = trackId;
+        this.trackName = trackName;
+        this.artistName = artistName;
+        this.albumName = albumName;
+        this.artworkUrl = artworkUrl;
+        this.previewUrl = previewUrl;
+        this.trackTime = trackTime;
+    }
     @Override
     public long getTrackId() {
-        return 0;
+        return trackId;
     }
 
     @Override
     public String getTrackName() {
-        return null;
+        return trackName;
     }
 
     @Override
     public String getArtistName() {
-        return null;
+        return artistName;
     }
 
     @Override
     public String getAlbumName() {
-        return null;
+        return albumName;
     }
 
     @Override
     public String getArtworkUrl() {
-        return null;
+        return artworkUrl;
     }
 
     @Override
     public String getPreviewUrl() {
-        return null;
+        return previewUrl;
     }
 
     @Override
     public long getTrackTime() {
-        return 0;
+        return trackTime;
     }
 
 }
